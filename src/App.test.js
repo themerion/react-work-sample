584c567bf852be49ac8c133d6def0f32bd609c20
Erik Lissel <erik@lissel.net>
2021-06-22 16:37:18 +0200

    Made the pre-installed test do _something_

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,10 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import React from "react";
 
-test("renders learn react link", () => {
+test("renders search label", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const linkElement = screen.getAllByText(/Search/i);
+  expect(linkElement).toBeTruthy();
 });
+
+/* TODO: Test search behaviour - in the fictional universe where this app will be further developed */
